a985b540357a936bd12b001c60e3f460c221f2b2
omar-ma66 <devcodefree66@gmail.com>
2026-06-15 22:13:29 +0200

    mise a jour sous linux prob bdd cles etrangeres

--- migrations/Version20260615130958.php
+++ migrations/Version20260615130958.php
@@ -17,18 +17,21 @@ final class Version20260615130958 extends AbstractMigration
         return '';
     }
 
-    public function up(Schema $schema): void
+ public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        // Table candidature
         $this->addSql('ALTER TABLE candidature ADD user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE candidature ADD CONSTRAINT FK_E33BD3B8A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_E33BD3B8A76ED395 ON candidature (user_id)');
-        $this->addSql('ALTER TABLE client_job ADD CONSTRAINT FK_DC7C735C19EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE client_job ADD CONSTRAINT FK_DC7C735CBE04EA9 FOREIGN KEY (job_id) REFERENCES job (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE job_candidature ADD CONSTRAINT FK_8057C8F1BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE job_candidature ADD CONSTRAINT FK_8057C8F1B6121583 FOREIGN KEY (candidature_id) REFERENCES candidature (id) ON DELETE CASCADE');
+        
+        // Table de liaison client_job (Déjà corrigée)
+        $this->addSql('ALTER TABLE client_job ADD CONSTRAINT FK_CLIENT_JOB_CLIENT FOREIGN KEY (client_id) REFERENCES client (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE client_job ADD CONSTRAINT FK_CLIENT_JOB_JOB FOREIGN KEY (job_id) REFERENCES job (id) ON DELETE CASCADE');
+        
+        // Table de liaison job_candidature (CORRECTIONS ICI)
+        $this->addSql('ALTER TABLE job_candidature ADD CONSTRAINT FK_JOB_CANDIDATURE_JOB FOREIGN KEY (job_id) REFERENCES job (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE job_candidature ADD CONSTRAINT FK_JOB_CANDIDATURE_CANDIDATURE FOREIGN KEY (candidature_id) REFERENCES candidature (id) ON DELETE CASCADE');
     }
-
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
